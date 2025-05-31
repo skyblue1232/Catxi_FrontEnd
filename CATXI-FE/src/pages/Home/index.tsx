fc745541e46b1d8405cecd85ac42bc086ca0c3d6
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-05-31 10:51:44 +0900

    fix/home

---
+++ CATXI-FE/src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import CategoryTab from './_components/CategoryTab';
+import LocationFilter from './_components/LocationFilter';
+import ChatCardList from './_components/ChatCardList';
+import FloatingButton from './_components/FloatingButton';
+import { useState } from 'react';
+
+const HomePage = () => {
+  const [selectedTab, setSelectedTab] = useState('학교에서 출발');
+  const [selectedLocations, setSelectedLocations] = useState<Record<string, string | null>>({
+    '학교에서 출발': null,
+    '학교에 도착': null,
+  });
+
+  const handleSelectLocation = (location: string) => {
+    setSelectedLocations((prev) => ({
+      ...prev,
+      [selectedTab]: location,
+    }));
+  };
+
+  return (
+    <div className="w-full">
+      <CategoryTab selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
+      <LocationFilter
+        selectedTab={selectedTab}
+        selectedLocation={selectedLocations[selectedTab]}
+        onSelectLocation={handleSelectLocation}
+      />
+      <ChatCardList />
+      <FloatingButton />
+    </div>
+  );
+};
+
+export default HomePage;
