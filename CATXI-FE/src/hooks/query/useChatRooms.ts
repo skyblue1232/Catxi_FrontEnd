6fa5d94fc29a2891120df6737bd1b37685cf97e2
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-09 18:38:03 +0900

    feat/chatRoom-query

---
+++ CATXI-FE/src/hooks/query/useChatRooms.ts
@@ -0,0 +1,24 @@
+// src/hooks/useChatRooms.ts
+import { useQuery } from '@tanstack/react-query';
+import { fetchChatRooms } from '../../apis/chatRooms';
+import type { ChatRoomResponse } from '../../types/chatData';
+
+interface GetChatRoomsParams {
+  direction: string;
+  station: string;
+  sort: string;
+  page?: number;
+}
+
+export const useChatRooms = (params: GetChatRoomsParams) => {
+  return useQuery<ChatRoomResponse, Error>({
+    queryKey: ['chatRooms', params],
+    queryFn: () => fetchChatRooms(params),
+    staleTime: 1000 * 60 * 1, 
+    refetchOnWindowFocus: false, 
+    refetchOnReconnect: false,
+    refetchOnMount: false, 
+    retry: 0, 
+    enabled: !!params.direction && !!params.station && !!params.sort, 
+  });
+};
