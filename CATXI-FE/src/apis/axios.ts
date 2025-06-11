4d9ae8f9524555459d95d76f90d2e62557fa4322
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-12 05:55:56 +0900

    chore/axios

--- CATXI-FE/src/apis/axios.ts
+++ CATXI-FE/src/apis/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import Storage from "../utils/storage";
 
+const token = Storage.getAccessToken(); 
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 axiosInstance.interceptors.response.use(
@@ -20,7 +25,7 @@ axiosInstance.interceptors.response.use(
     ) {
       alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
       Storage.clearStorage();
-      window.location.href = "/login";
+      window.location.href = "/";
     }
     if (status === 500) {
       alert("서버 에러가 발생했습니다. 잠시 후 다시 시도해주세요.");
