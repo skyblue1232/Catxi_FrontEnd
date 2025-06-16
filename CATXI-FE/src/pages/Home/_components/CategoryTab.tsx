e12fd32a756f9a2b48288f4de1e17f4590f566d4
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-17 07:51:22 +0900

    Update CategoryTab.tsx

--- CATXI-FE/src/pages/Home/_components/CategoryTab.tsx
+++ CATXI-FE/src/pages/Home/_components/CategoryTab.tsx
@@ -10,7 +10,7 @@ const tabs = [
 
 const CategoryTab = ({ direction, setDirection }: Props) => {
   return (
-    <div className="flex w-full justify-center border-b-[1px] border-[#E0E0E0] mt-[1.25rem]">
+    <div className="flex w-full justify-center border-b-[1px] border-[#E0E0E0] mt-[1.75rem]">
       {tabs.map((tab) => (
         <div key={tab.value} className="relative">
           <button
