0e5a09d1a751c3760b3df2e7ee6c1ad243288928
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-18 22:49:28 +0900

    fix/chat-input

--- CATXI-FE/src/layouts/ChatLayout.tsx
+++ CATXI-FE/src/layouts/ChatLayout.tsx
@@ -77,7 +77,7 @@ const ChatLayout = () => {
       <div className="flex-1 overflow-y-auto pb-[80px]">
         <Outlet context={contextValue} />
       </div>
-      <div className="absolute bottom-0 left-0 w-full border-t border-b border-gray-300 bg-background px-[1.656rem] pt-[0.625rem] pb-[0.938rem] z-100 flex-1">
+      <div className="fixed bottom-0 left-0 w-full border-t border-b border-gray-300 bg-background px-[1.656rem] pt-[0.625rem] pb-[0.938rem] z-100 flex-1">
         <ChatInput value={input} onChange={setInput} onSubmit={handleSubmit} />
       </div>
     </div>
